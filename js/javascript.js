f16291032a737e1a04d3a80a48588292fe69b536
Sithag <simon.destanque@mail.com>
2018-11-22 15:02:37 +0100

    Final

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+	$(".Rbt").html("<h2>Rambouillet - France</h2> <h3>Appartement 4 pièces</h3>");
+	$(".Paris").html("<h2>Paris - France</h2> <h3>Appartement 2 pièces</h3>");
+	$(".Bnk").html("<h2>Bangkok - Thailande</h2><h3>Appartement 7 pièces</h3>");
+	$(".Seoul").html("<h2>Séoul - Corée du Sud</h2><h3>Appartement 4 pièces</h3>");
+	$(".Tky").html("<h2>Tokyo - Japon</h2><h3>Appartement 4 pièces</h3>");
+	$(".Tel").html("<h2>Tel-Aviv - Israël</h2><h3>Appartement 5 pièces</h3>");
+	$(".Pekin").html("<h2>Pékin - Chine</h2><h3>Appartement 0.5 pièces</h3>");
+	$(".Berlin").html("<h2>Berlin - Allemagne</h2><h3>Appartement 2 pièces</h3>");
+	$(".New-York").html("<h2>New-York - USA</h2><h3>Appartement 1 pièces</h3>");
+	$(".Rio").html("<h2>Rio de janeiro - Brésil</h2><h3>Appartement 4 pièces</h3>");
